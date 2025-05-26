b321df989be658043377daa0b2b1616f26ae0f69
Sanjeev-7714 <sanjeev.dev7714@gmail.com>
2025-05-26 19:49:06 +0530

    Fourth Commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.JPG'],
+  assetsInclude: ['**/*.jpg', '**/*.JPG', '**/*.png', '**/*.svg'],
   build: {
     chunkSizeWarningLimit: 1000, // Increase size limit to 1000kb
     rollupOptions: {
@@ -14,6 +15,13 @@ export default defineConfig({
           // Add other third-party dependencies here if needed
         },
       },
+    },
+    assetsDir: 'assets',
+    sourcemap: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
